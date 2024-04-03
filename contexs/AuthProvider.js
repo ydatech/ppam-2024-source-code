6357b9766c9e41c55c56e5f31c2f091a8855e6a6
Yuda Sukmana <yudasukmana1992@gmail.com>
2024-04-04 05:50:55 +0700

    auth provider using firebase auth

--- contexs/AuthProvider.js
+++ contexs/AuthProvider.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import auth from '@react-native-firebase/auth';
 
 const AuthContext = React.createContext();
 
@@ -10,17 +11,45 @@ export function useAuth(){
 
 export default function AuthProvider({children}){
     const [isLoggedIn,setIsLoggedIn] = useState(false);
+    // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
 
-    const signIn = ()=>{
+    // Handle user state changes
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if(!user)
+        setIsLoggedIn(false);
+        else
         setIsLoggedIn(true);
+
+        if (initializing) setInitializing(false);
+    }
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
+
+    const signIn = (email,password)=>{
+        // setIsLoggedIn(true);
+        auth().signInWithEmailAndPassword(email,password);
+    }
+
+    const signUp = (email,password) =>{
+        auth().signInWithEmailAndPassword(email,password);
     }
 
     const signOut = ()=>{
-        setIsLoggedIn(false);
+        // setIsLoggedIn(false);
+        auth().signOut();
     }
+
+    console.log("user",user)
 
     return <AuthContext.Provider value={{
         isLoggedIn,
+        user,
         signIn,
         signOut
     }}>
